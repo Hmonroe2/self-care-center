7078ee494624d38784cff94dd48864d2ecf4b2df
Hunter Monroe <huntis@outlook.com>
2022-05-29 16:11:31 -0600

    Refactored Code

--- src/main.js
+++ src/main.js
@@ -1,42 +1,3 @@
-var affirmations = [
- `I forgive myself and set myself free.`,
- `I believe I can be all that I want to be.`,
- `I am in the process of becoming the best version of myself.`,
- `I have the freedom & power to create the life I desire.`,
- `I choose to be kind to myself and love myself unconditionally.`,
- `My possibilities are endless.`,
- `I am worthy of my dreams.`,
- `I am enough.`,
- `I deserve to be healthy and feel good.`,
- `I am full of energy and vitality and my mind is calm and peaceful.`,
- `Every day I am getting healthier and stronger.`,
- `I honor my body by trusting the signals that it sends me.`,
- `I manifest perfect health by making smart choices.`,
-]
-
-var mantras = [
- `Breathing in, I send myself love. Breathing out, I send love to someone else who needs it.`,
- `Don\'t let yesterday take up too much of today.`,
- `Every day is a second chance.`,
- `Tell the truth and love everyone.`,
- `I am free from sadness.`,
- `I am enough.`,
- `In the beginning it is you, in the middle it is you and in the end it is you.`,
- `I love myself.`,
- `I am present now.`,
- `Inhale the future, exhale the past.`,
- `This too shall pass.`,
- `Yesterday is not today.`,
- `The only constant is change.`,
- `Onward and upward.`,
- `I am the sky, the rest is weather.`,
-]
-
-
-
-
-
-
 
 var recieveMessageButton = document.querySelector(".recieve-button1")
 var radioAffirmButton = document.querySelector("#affirmationButton")
@@ -47,14 +8,8 @@ var yogaguy = document.querySelector(".yoga-Guy")
 
 
 
-// window.addEventListener("load", yogaGuyPicture)
-recieveMessageButton.addEventListener("click", viewMessage)
-// radioAffirmButton.addEventListener("", viewMessage)
- // radioMantraButton.addEventListener("click", viewMessage)
-
-
-// yogaguy.classList.hidden(true)
-
+radioAffirmButton.addEventListener("click", showAffirm)
+radioMantraButton.addEventListener("click", showMantra)
 
 
 function getRandomItemFromArray(array) {
@@ -62,30 +17,19 @@ function getRandomItemFromArray(array) {
   return array[randomArrayIndex]
 }
 
-
-function viewMessage(){
-    radioAffirmButton.addEventListener("", viewMessage)
+function showAffirm(){
+    recieveMessageButton.addEventListener("click", showAffirm)
     mantraText.innerHTML = (getRandomItemFromArray(affirmations))
-
+    hide()
+    console.log("i am also working")
 }
 
+function showMantra(){
+  recieveMessageButton.addEventListener("click", showMantra)
+  mantraText.innerHTML = (getRandomItemFromArray(mantras))
+console.log("im working")
+}
 
-// showMantra(){
-//   if("click"=== true){
-//     return true
-//   }
-// }
-//
-// function showMantra(){
-//
-// }
-// function showAffirmation(){
-//   mantraText.innerText = (getRandomItemFromArray(affirmations))
-//
-// }
-
-
-// function viewMessage(){
-//   radioMantraButton.addEventListener("click", viewMessage)
-//    mantraText.innerHTML=(getRandomItemFromArray(mantras))
-// }
+function hide(){
+  // yogaguy.classList.add("hidden")
+}
